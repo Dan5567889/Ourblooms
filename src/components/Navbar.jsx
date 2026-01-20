238390f356b87fc9cf1981fa736f6b072cdfafca
Dan5567889 <dantehernandz587@gmail.com>
2026-01-20 09:15:01 -0500

    fixed mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,48 @@
 import styles from "../styles/Navbar.module.css";
 import { Link } from "react-router-dom";
+import { Menu } from 'lucide-react';
+import { X } from 'lucide-react';
+import {useState} from 'react';
 
 function Navbar(){
+    const [isOpen, setIsOpen] = useState(false);
+    function toggle(){
+        setIsOpen(!isOpen);
+    }
     return (
         <>
         <nav className={styles.navbar}>
-            <p className="display"> Our Blooms </p>
+            <p className="display"> <Link to='/'> OUR BLOOMS </Link> </p>
+            
             <ul className={styles.ul}>
-                
                 <li className="caption1" > <Link to='/gallery'> GALLERY </Link> </li>
                 <li className="caption1" > <Link to='/about'> ABOUT </Link> </li>
-                <li className="caption1" > <Link to='/mailto:dantehernandez587@gmail.com'> CONTACT </Link> </li>
+                <li className="caption1">
+                  <a href="mailto:dantehernandez587@gmail.com">CONTACT</a>
+                </li>
             </ul>
         </nav>
+
+
+        <nav className={styles.mobile_navbar}>
+            <p className="display"> <Link to='/'> O.B </Link> </p>
+            {isOpen? <X onClick={toggle}/>:<Menu onClick={toggle}/>}
+            
+        </nav>
+
+        {/* This is a classical and gate example */}
+        {isOpen && (
+            <div> 
+                <ul className={styles.mobile_ul}>
+                <li className="caption1"  > <Link to='/gallery'> GALLERY </Link> </li>
+                <li className="caption1" > <Link to='/about'> ABOUT </Link> </li>
+                <li className="caption1">
+                  <a href="mailto:dantehernandez587@gmail.com">CONTACT</a>
+                </li>
+            </ul>
+            </div>)}
+            
+        
         <hr className='divider'/>
         </>
     )
